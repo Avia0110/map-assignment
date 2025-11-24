fd1742fb89effeb5d73b5cb243d7205bc56cac59
avia genossar <aviagenossar@avias-MacBook-Pro.local>
2025-11-24 21:31:23 +0200

    update: added note in state.py

--- backend/app/state.py
+++ backend/app/state.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from .models.schemas import AxisRange, MapImage, Mode2Calibration
 
+# TEMPORARY USE OF GLOBAL VARIABLES
+# NOTE TO SELF: TRY TO CHANGE AFTER XML INTEGRATION
 axis_range: Optional[AxisRange] = None
 map_image: Optional[MapImage] = None
 ###MODE2
